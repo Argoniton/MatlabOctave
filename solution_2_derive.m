87269459cbace92eb108b651bd79077758ff4d01
Octave Online <webmaster@octave-online.net>
2018-02-27 08:03:35 +0000

    Scripted user file commit

--- solution_2_derive.m
+++ solution_2_derive.m
@@ -1,7 +1,7 @@
 disp("f(x)=(sin(x))^2; -u''(x)=f(x), 0<x<L; u(0)=u(L)=0");
 
 L = 10;
-N = 1000;
+N = 100;
 h = L/(N+1);
 M = 2*diag(ones(N,1),0) - diag(ones(N-1,1),1) - diag(ones(N-1,1),-1);
 M = M/h^2;
