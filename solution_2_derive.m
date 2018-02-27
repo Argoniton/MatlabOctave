8aea831fc85f795cd8b5a952396b782f2c999362
Octave Online <webmaster@octave-online.net>
2018-02-27 08:02:29 +0000

    Scripted auto-commit

--- solution_2_derive.m
+++ solution_2_derive.m
@@ -1,7 +1,12 @@
-disp("f(x)=(sin(x))^2");
+disp("f(x)=(sin(x))^2; -u''(x)=f(x), 0<x<L; u(0)=u(L)=0");
+
 L = 10;
-N = 100;
+N = 1000;
 h = L/(N+1);
-M = 2*diag(ones(N,1),0)-diag(ones(N-1,1),1)-diag(ones(N-1,1),-1);
+M = 2*diag(ones(N,1),0) - diag(ones(N-1,1),1) - diag(ones(N-1,1),-1);
 M = M/h^2;
-x=[h:h:L-h]'
+X = [h:h:L-h]'; # debut:plus:fin, prime -> transpose
+F = (sin(X)).^2;
+# MU=F => U=F/M <- resulution de sys lineaire
+U = M\F;
+plot(X,U)
