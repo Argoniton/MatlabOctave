299131f7a942297e66afd3c7f39678c6bc758883
Octave Online <webmaster@octave-online.net>
2018-02-27 07:31:34 +0000

    Scripted auto-commit

--- learn_syntaxis.m
+++ learn_syntaxis.m
@@ -1,5 +1,10 @@
+#{
 disp("Hello World");
 tab = [1,2,3;2,3,4];
 tab*[1;3;4] % prod matr.
 tab.*tab % composant * composant
-[2,3].*[3,4]
+[2,3].*[3,4] % =[6;12], aussi ./ et .^ 
+#}
+
+diag([1;2;4;5],5,7)
+diag([1;2;4;5],3)
