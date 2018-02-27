69e91fbdcc5f6c11a9d4503c57cdf780deaa9e31
Octave Online <webmaster@octave-online.net>
2018-02-27 07:42:35 +0000

    Scripted auto-commit

--- solution_2_derive.m
+++ solution_2_derive.m
@@ -1,7 +1,8 @@
-disp("Hello World");
-L=10;
-N=100;
-h=L\(N+1);
-M=2*diag(ones(N,1),0)
-M=M/h^2;
-tab = [1,2,3;2,3,4];
+disp("f(x)=(sin(x))^2");
+L = 10;
+N = 100;
+h = L\(N+1)
+M = 2*diag(ones(N,1),0)-diag(ones(N-1,1),1)-diag(ones(N-1,1),-1);
+M = M/h^2;
+x=[0.1:0.1:9]';
+x';
