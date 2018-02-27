8ca3c991cf96add1f7e22d9baee6fe690c016e67
Octave Online <webmaster@octave-online.net>
2018-02-27 08:08:15 +0000

    Scripted user file commit

--- solution_2_derive.m
+++ solution_2_derive.m
@@ -6,6 +6,92 @@ h = L/(N+1);
 M = 2*diag(ones(N,1),0) - diag(ones(N-1,1),1) - diag(ones(N-1,1),-1);
 M = M/h^2;
 X = [h:h:L-h]'; # debut:plus:fin, prime -> transpose
+X'
+F = (sin(X)).^2;
+# MU=F => U=F/M <- resulution de sys lineaire
+U = M\F;
+plot(X,U)
+
+L = 10;
+N = 100;
+h = L/(N+1);
+M = 2*diag(ones(N,1),0) - diag(ones(N-1,1),1) - diag(ones(N-1,1),-1);
+M = M/h^2;
+X = [h:h:L-h]'; # debut:plus:fin, prime -> transpose
+X'
+F = (sin(X)).^2;
+# MU=F => U=F/M <- resulution de sys lineaire
+U = M\F;
+plot(X,U)
+
+L = 10;
+N = 100;
+h = L/(N+1);
+M = 2*diag(ones(N,1),0) - diag(ones(N-1,1),1) - diag(ones(N-1,1),-1);
+M = M/h^2;
+X = [h:h:L-h]'; # debut:plus:fin, prime -> transpose
+X'
+F = (sin(X)).^2;
+# MU=F => U=F/M <- resulution de sys lineaire
+U = M\F;
+plot(X,U)
+
+L = 10;
+N = 100;
+h = L/(N+1);
+M = 2*diag(ones(N,1),0) - diag(ones(N-1,1),1) - diag(ones(N-1,1),-1);
+M = M/h^2;
+X = [h:h:L-h]'; # debut:plus:fin, prime -> transpose
+X'
+F = (sin(X)).^2;
+# MU=F => U=F/M <- resulution de sys lineaire
+U = M\F;
+plot(X,U)
+
+L = 10;
+N = 100;
+h = L/(N+1);
+M = 2*diag(ones(N,1),0) - diag(ones(N-1,1),1) - diag(ones(N-1,1),-1);
+M = M/h^2;
+X = [h:h:L-h]'; # debut:plus:fin, prime -> transpose
+X'
+F = (sin(X)).^2;
+# MU=F => U=F/M <- resulution de sys lineaire
+U = M\F;
+plot(X,U)
+
+L = 10;
+N = 100;
+h = L/(N+1);
+M = 2*diag(ones(N,1),0) - diag(ones(N-1,1),1) - diag(ones(N-1,1),-1);
+M = M/h^2;
+X = [h:h:L-h]'; # debut:plus:fin, prime -> transpose
+X'
+F = (sin(X)).^2;
+# MU=F => U=F/M <- resulution de sys lineaire
+U = M\F;
+plot(X,U)
+
+
+L = 10;
+N = 100;
+h = L/(N+1);
+M = 2*diag(ones(N,1),0) - diag(ones(N-1,1),1) - diag(ones(N-1,1),-1);
+M = M/h^2;
+X = [h:h:L-h]'; # debut:plus:fin, prime -> transpose
+X'
+F = (sin(X)).^2;
+# MU=F => U=F/M <- resulution de sys lineaire
+U = M\F;
+plot(X,U)
+
+L = 10;
+N = 100;
+h = L/(N+1);
+M = 2*diag(ones(N,1),0) - diag(ones(N-1,1),1) - diag(ones(N-1,1),-1);
+M = M/h^2;
+X = [h:h:L-h]'; # debut:plus:fin, prime -> transpose
+X'
 F = (sin(X)).^2;
 # MU=F => U=F/M <- resulution de sys lineaire
 U = M\F;
