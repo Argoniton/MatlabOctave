3f06e30c2767eb95dd601cd75d5df269b353e5b9
Octave Online <webmaster@octave-online.net>
2018-02-28 09:39:51 +0000

    Scripted auto-commit

--- resolue_le_pb_de_Dirichlet_1d.m
+++ resolue_le_pb_de_Dirichlet_1d.m
@@ -1,10 +1,10 @@
 disp("f(x)=(exp(x))^2; -u''(x)=f(x), 0<x<L; u(0)=u(L)=0");
 
 L = 10;
-N = 150;
+N = 15;
 h = L/(N+1);
-M = 2*diag(ones(N,1),0) - diag(ones(N-1,1),1) - diag(ones(N-1,1),-1)
-M = M/h^2;
+M = 2*diag(ones(N,1),0) - diag(ones(N-1,1),1) - diag(ones(N-1,1),-1);
+M = M/h^2
 X = [h:h:L-h]'; # debut:plus:fin, prime -> transpose
 F = (exp(X)).^2;
 # MU=F => U=F/M <- resulution de sys lineaire
