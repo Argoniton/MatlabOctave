012660e5b170a4d247515073aa3feefa6d15888d
Octave Online <webmaster@octave-online.net>
2018-03-13 07:37:12 +0000

    Scripted auto-commit

--- my_script.m
+++ my_script.m
@@ -1,1 +1,18 @@
-disp("Hello World");
+#{
+-Δu=f sur Ω=]0,L_x[*]0,L_y[
+u=α sur Γ_1
+∂_n u=0 sur Γ_2
+    _____
+    |   |
+ -> |   |
+    _____
+      ^
+      |
+
+Nh=L_x
+h=L_x/N
+
+Ml=L_y
+l=L_y/M
+#}
+
